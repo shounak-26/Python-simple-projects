e2cee02fee98f3924fa459b0eabec17065576abf
shounak-26 <93966554+shounak-26@users.noreply.github.com>
2021-11-09 11:51:55 +0530

    Mad-lib generator

---
+++ Mad-lab generator.py
@@ -0,0 +1,7 @@
+color = input("Enter the color:")
+noun = input("Enter noun:")
+celebrity = input("Enter the name:")
+
+print("Roses are", color ,"in color" )
+print(noun , "are blue")
+print(celebrity , "is a popular celebrity")
